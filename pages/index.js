351432d03f358a4e4e1b8f00b086f712d631e3e9
Jacob Ford <j@cobford.com>
2021-06-17 20:36:50 -0400

    Update site title

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import { getArtifactBySlug, getAllSlugs } from '../lib/api'
 export default function TableOfContents({ pieces }) {
   return (
     <article>
-      <h1 className="site-title">Documentation of Jacob Ford</h1>
-      <h2 className="site-subtitle">All substance, no style</h2>
+      <h1 className="site-title">The COVID Material Culture Museum</h1>
+      <h2 className="site-subtitle">Pandemic is the mother of invention</h2>
       <ul>
       {pieces.map(piece =>
         <li>{piece.title}</li>
